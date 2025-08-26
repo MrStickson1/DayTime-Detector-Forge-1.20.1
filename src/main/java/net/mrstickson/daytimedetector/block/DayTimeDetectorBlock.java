9ba2edf9a1f3f77b1bda46e61024a35729797a3f
MrStickson1 <06burak2008@gmail.com>
2025-08-26 15:29:32 +0300

    Added: Block model and texture.

--- src/main/java/net/mrstickson/daytimedetector/block/DayTimeDetectorBlock.java
+++ src/main/java/net/mrstickson/daytimedetector/block/DayTimeDetectorBlock.java
@@ -4,15 +4,33 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class DayTimeDetectorBlock extends Block {
 
+    public static final BooleanProperty INVERTED = BooleanProperty.create("inverted");
+
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D);
+
     public DayTimeDetectorBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(INVERTED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(INVERTED);
     }
 
     @Override
@@ -34,15 +52,28 @@ public class DayTimeDetectorBlock extends Block {
     }
 
     @Override
-    public int getSignal(BlockState blockState, BlockGetter blockAccess, BlockPos pos, Direction side) {
+    public int getSignal(BlockState state, BlockGetter blockAccess, BlockPos pos, Direction side) {
         if (!(blockAccess instanceof ServerLevel world)) return 0;
 
         long time = world.getServer().getLevel(Level.OVERWORLD).getDayTime() % 24000;
+        boolean inverted = state.getValue(INVERTED);
 
-        if (time >= 12300 && time <= 23850) {
-            return 0;
-        } else {
-            return 15;
+        int signal = (time >= 12300 && time <= 23850) ? 0 : 15;
+        return inverted ? 15 - signal : signal;
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!world.isClientSide) {
+            state = state.cycle(INVERTED);
+            world.setBlock(pos, state, 3);
+            world.updateNeighborsAt(pos, this);
         }
+        return InteractionResult.sidedSuccess(world.isClientSide);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 }
